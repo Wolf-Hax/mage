31413211c2af05c230c1e00e5e894449291145ce
wolfhax <itswolfhax@gmail.com>
2022-04-24 09:33:45 +0500

    DAY3

--- lib/Pages/Loginpage.dart
+++ lib/Pages/Loginpage.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: file_names
+// ignore_for_file: file_names, prefer_const_constructors
 
 import 'package:flutter/material.dart';
 
@@ -8,12 +8,18 @@ class Loginpage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
+        color: Colors.white,
         child: Column(
-      children: [
-        Image.asset(
-          "assets/Images/Login_image.png",
-        )
-      ],
-    ));
+          children: [
+            Image.asset(
+              "assets/Images/Login_image.png",
+              fit: BoxFit.cover,
+            ),
+            Text(
+              "LOGIN",
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            )
+          ],
+        ));
   }
 }
